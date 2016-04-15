965780e47a54bb76e9bc2b987e42e536deeead9a
aleksey.tanana <aleksey.tanana@iksatprof.ru>
2016-04-15 15:55:23 +0200

    fixture start end separation

--- fixture/application.py
+++ fixture/application.py
@@ -11,6 +11,13 @@ class Application:
         self.session = SessionHelper(self)
         self.channel = ChannelHelper(self)
 
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
     def open_home_page(self):
         wd = self.wd
         wd.get("http://10.130.8.116/epg/")
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,22 @@ import pytest
 from fixture.application import Application
 
 
-@pytest.fixture(scope = "session")
+fixture = None
+
+@pytest.fixture
 def app(request):
-    fixture = Application()
-    fixture.session.login("root", "123")
+    global fixture
+    if fixture is None:
+        fixture = Application()
+        fixture.session.login("root", "123")
+    elif not fixture.is_valid():
+        fixture = Application()
+        fixture.session.login("root", "123")
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
     def fin():
         fixture.session.logout()
         fixture.destroy()
